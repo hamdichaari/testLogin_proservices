5d20892f69e8f2b1017a738e6a2af9377e816fbc
extrainfo <extrainfo@DESKTOP-JJ8J2OK>
2022-02-09 17:55:15 +0100

    test non valide login proservices

--- src/LoginPageTest.java
+++ src/LoginPageTest.java
@@ -1,5 +1,7 @@
 
 
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,7 +9,7 @@ import org.testng.annotations.Test;
 
 public class LoginPageTest {
 
-	@Test 
+	@Test (priority=2)
 	public void TestLogin()
 	{
 
@@ -27,9 +29,31 @@ public class LoginPageTest {
 	
 	}
 	
+	@Test (priority=1)
+	public void TestLoginFieled()
+	{
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\extrainfo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		String url ="https://proservices-training-company.com/" ;
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get(url);
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//li[@class='menu-right']//a[@class='login js-show-popup'][normalize-space()='Login']")).click();
+	    driver.findElement(By.xpath("//input[@placeholder='Identifiant']")).sendKeys("hamdich");
+	    driver.findElement(By.xpath("//input[@name='pwd']")).sendKeys("Extra");
+	    driver.findElement(By.xpath("//input[@value='Connexion']")).click();
+	    
+	   String pass = driver.findElement(By.xpath("//input[@value='Connexion']")).getText();
+	   
+	    assertEquals(pass,"The password is incorrect");
 	
 	
 	
+	}
 	
 	
 	
